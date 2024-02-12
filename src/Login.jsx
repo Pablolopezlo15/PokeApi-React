dd1bbbb29524059316f705ddf769727a160cefaa
Pablo Lopez Lozano <plopezlozano12@gmail.com>
2024-02-12 11:52:19 +0100

    Form

--- src/Login.jsx
+++ src/Login.jsx
@@ -2,6 +2,7 @@ import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { getAuth } from "firebase/auth";
 import auth from "./firebase";
 import { useNavigate } from "react-router-dom";
+import './assets/css/Login.css';
 
 function Login() {
   const navigate = useNavigate();
@@ -35,9 +36,45 @@ function Login() {
   };
 
   return (
-    <div>
-      <button onClick={signInWithGoogle}>Sign in with Google</button>
+    <>
+    <div className="container-form">
+        <div className="wrapper">
+            <div className="card-switch">
+                <label className="switch">
+                    <input type="checkbox" className="toggle" />
+                    <span className="slider"></span>
+                    <span className="card-side"></span>
+                    <div className="flip-card__inner">
+                        <div className="flip-card__front">
+                            <div className="title">Log in</div>
+                                <form className="flip-card__form" action="">
+                                    <input className="flip-card__input" name="email" placeholder="Email" type="email" />
+                                    <input className="flip-card__input" name="password" placeholder="Password" type="password" />
+                                    <button className="flip-card__btn">Let`s go!</button>
+                                </form>
+                            </div>
+                            
+                            <div className="flip-card__back">
+                                <div className="title">Sign up</div>
+                                    <form className="flip-card__form" action="">
+                                        <input className="flip-card__input" placeholder="Name" type="text" />
+                                        <input className="flip-card__input" name="email" placeholder="Email" type="email" />
+                                        <input className="flip-card__input" name="password" placeholder="Password" type="password" />
+                                        <button className="flip-card__btn">Confirm!</button>
+                                    </form>
+                                </div>
+                            </div>
+                </label>
+            </div>
+        </div>
     </div>
+      
+
+       <div>
+          <button onClick={signInWithGoogle}>Sign in with Google</button>
+        </div>
+    </>
+
   );
 }
 
